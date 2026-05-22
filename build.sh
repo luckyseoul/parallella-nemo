ee4698d6ae1be3de3a24517e6870df52be674753
Nicholas Perry <nick@perrybrandsllc.com>
2026-05-22 22:39:49 +0000

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Build script for Parallella Nemo
-
 set -e
 
 BR2_EXTERNAL=$(pwd)/external/parallella
@@ -8,15 +7,16 @@ DEFCONFIG=$BR2_EXTERNAL/defconfig
 
 echo "Building Parallella Nemo..."
 
-# This assumes Buildroot is cloned at ../buildroot
 if [ ! -d "../buildroot" ]; then
     echo "Error: Buildroot not found at ../buildroot"
-    echo "Please clone Buildroot first:"
+    echo "Please clone Buildroot:"
     echo "  git clone https://gitlab.com/buildroot.org/buildroot.git ../buildroot"
     exit 1
 fi
 
-make -C ../buildroot BR2_EXTERNAL=$BR2_EXTERNAL defconfig BR2_DEFCONFIG=$DEFCONFIG
+make -C ../buildroot BR2_EXTERNAL=$BR2_EXTERNAL \
+    defconfig BR2_DEFCONFIG=$DEFCONFIG
+
 make -C ../buildroot BR2_EXTERNAL=$BR2_EXTERNAL
 
-echo "Build complete."
+echo "Build complete. Images are in ../buildroot/output/images/"
