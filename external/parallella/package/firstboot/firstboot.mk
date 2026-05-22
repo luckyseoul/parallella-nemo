9caa2c6ce52ed4317459e64d2801dc7a28a38c66
Nicholas Perry <nick@perrybrandsllc.com>
2026-05-22 22:26:20 +0000

    Add firstboot and etop as Buildroot packages

---
+++ external/parallella/package/firstboot/firstboot.mk
@@ -0,0 +1,14 @@
+FIRSTBOOT_VERSION = 1.0
+FIRSTBOOT_SITE = $(TOPDIR)/../scripts/firstboot
+FIRSTBOOT_SITE_METHOD = local
+
+define FIRSTBOOT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/installer.py $(TARGET_DIR)/usr/lib/firstboot/installer.py
+	$(INSTALL) -D -m 0644 $(@D)/parallella-firstboot.service $(TARGET_DIR)/usr/lib/systemd/system/parallella-firstboot.service
+endef
+
+define FIRSTBOOT_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(@D)/parallella-firstboot.service $(TARGET_DIR)/usr/lib/systemd/system/parallella-firstboot.service
+endef
+
+$(eval $(generic-package))
